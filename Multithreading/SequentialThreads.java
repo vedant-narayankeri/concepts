bdfff318d27da4432eef502ccc18d34300b69e30
Vedant Narayankeri <nsvedant2000@gmail.com>
2026-06-02 01:31:41 +0530

    description

--- Multithreading/SequentialThreads.java
+++ Multithreading/SequentialThreads.java
@@ -11,6 +11,11 @@ public class SequentialThreads
         Thread t1 = new Thread(() -> printInOrder(1, "Thread-1"));
         Thread t2 = new Thread(() -> printInOrder(2, "Thread-2"));
         Thread t3 = new Thread(() -> printInOrder(3, "Thread-3"));
+        //thread.start() -> JVM creates new OS thread -> Internally calls thread.run() -> Executes runnable lambda supplied
+        //Note - For a busy/multi-core CPU, T2, T3 could win race. Even though usually T1 runs first but we never know
+        //Analogy -> Printer submitted 3 jobs, potentially J2 runs before as the file is smaller and hence downloads faster
+        //Our job is to submit the Job, but Printer decides the order
+        //We use synchronization for the Guarantee 
         t1.start();
         t2.start();
         t3.start();
@@ -18,16 +23,20 @@ public class SequentialThreads
 
     private static void printInOrder(int threadNumber, String threadName) {
        synchronized(lock){
+            //Need while() loop because there is possibility of Spurious wakeups
+            //JVM spec allows threads to wake up randomly even without noitfy() being called
+            //In case we put logic in if() condition, then we just exit if and execute the code below, leading to random behaviour
+            //Java concurrency guide - Standard rule
             while(threadNumber != currentThread) {
                 try {
-                    lock.wait();
+                    lock.wait();//release lock and wait
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
             }
             System.out.println(threadName + " executed");
             currentThread++;
-            lock.notifyAll();;
+            lock.notifyAll();//Wake up all waiting threads
        }
     }
 }
