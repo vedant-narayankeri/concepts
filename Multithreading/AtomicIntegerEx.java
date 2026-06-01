97783687112c7a9abe7d97643cb6e7463c147f19
Vedant Narayankeri <nsvedant2000@gmail.com>
2026-06-02 03:08:32 +0530

    atomic integer

---
+++ Multithreading/AtomicIntegerEx.java
@@ -0,0 +1,25 @@
+package multithreading;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerEx {
+    private static final AtomicInteger counter = new AtomicInteger(0);
+    
+    public static void main(String[] args) throws InterruptedException{
+        Thread t1 = new Thread(() -> increment("Thread-1"));
+        Thread t2 = new Thread(() -> increment("Thread-2"));
+        Thread t3 = new Thread(() -> increment("Thread-3"));
+        t1.start();
+        t2.start();
+        t3.start();
+        t1.join();
+        t2.join();
+        t3.join();
+        System.out.println("Final count: " + counter.get());
+    }
+
+    private static void increment(String threadName) {
+        counter.incrementAndGet();//Similar to ++counter
+        System.out.print(threadName + " done \n");
+    }
+}
