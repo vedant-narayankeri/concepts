33e972158b75fea94419a97ee2ae1fe1ac1f1447
Vedant Narayankeri <nsvedant2000@gmail.com>
2026-06-11 12:27:01 +0530

    something abourt futures

--- multithreading/FuturesEx.java
+++ multithreading/FuturesEx.java
@@ -1,2 +1,27 @@
 package multithreading;
 //TODO - Futures, CompletableFuture, ExecutorService. Compare and contrast
+
+/***
+ * Futures - blocking & limited
+ * 
+ * ExecutorService exec = Executors.newFixedThreadPool(4);
+ * Future<Integer> future = exec.submit(() -> //Do something);
+ * int result = future.get();// Blocks forever
+ * int result = future.get(5, TimeUnit.SECONDS);// Blocks with timeout
+ * 
+ * Functions
+ * get() - block and wait
+ * isDone() - poll
+ * cancel() - attempt cancelled
+ * 
+ * 
+ * CompletableFuture - Non blocking, chainable
+ * 
+ * CompleatableFuture<User> userF = CompletableFuture.supplyAsync(() => fetchUser(42));
+ * 
+ * We can chain callbacks with thenApply()/thenCompose()
+ * We can combine paralle tasks with allOf()/thenCombine
+ * Handle elrrors with exceptionally()
+ * 
+ * We can do all of above without blocking a thread
+ * **/
