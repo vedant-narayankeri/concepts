5001e8a4408b9d70e4911132e565c157bde3809a
Vedant Narayankeri <nsvedant2000@gmail.com>
2026-06-11 12:07:57 +0530

    brief

--- java/DataTypes.java
+++ java/DataTypes.java
@@ -1,1 +1,5 @@
 package java;
+
+// int - whole numbers, 4 Bytes (-2.1 Billion to + 2.1 Billion)
+// long - whole numbers, 8 bytes (-9.2 quintillion to + 9.2 quintillion)
+// double - decimal numbers, 8 bytes (~15 decimal digits precision)
